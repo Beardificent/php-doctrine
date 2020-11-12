95e4a82df5c0a34a0fb6e1d33f2a102651cdfaa7
Beardificent <cis.magito@gmail.com>
2020-11-12 16:54:43 +0100

    added a NotNUll message at email

--- src/Form/Type/StudentType.php
+++ src/Form/Type/StudentType.php
@@ -5,14 +5,16 @@ namespace App\Form\Type;
 
 
 use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotNull;
 
-class StudentType extends \Symfony\Component\Form\AbstractType
+class StudentType extends AbstractType
 {
 
     public function buildForm(FormBuilderInterface $builder, array $options) : void
@@ -30,7 +32,10 @@ class StudentType extends \Symfony\Component\Form\AbstractType
             ])
             ->add('email', EmailType::class,[
                 'constraints' => [
-                    new NotNull(),
+                    new NotNull([
+                        'message' => 'Not a valid email format'
+                    ]),
+                    new Email()
                 ]
             ])
             ->add('address_street', TextType::class,[
