495f2de526d758366a25847bde5a4e38fdd77dae
Beardificent <cis.magito@gmail.com>
2020-11-12 11:52:00 +0100

    https://www.thinktocode.com/2018/03/26/symfony-4-rest-api-part-1-fosrestbundle/ tutorial im following

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+//tutotial https://www.thinktocode.com/2018/03/26/symfony-4-rest-api-part-1-fosrestbundle/
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index(SerializerInterface $serializer): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
